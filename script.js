4657d316e1ee3884c442762f49b59e7166753c44
Yohannes Dawit <yohannesabreham08@gmail.com>
2025-02-17 01:56:49 +0300

    update

--- script.js
+++ script.js
@@ -5,30 +5,30 @@ document.addEventListener('DOMContentLoaded', function(){
 
             const feedbackDiv = document.getElementById('form-feedback');
     
-            const password = document.getElementById('password');
-            const passwordNew = password.value.trim();
+            const passwordNew = document.getElementById('password');
+            const password = passwordNew.value.trim();
         
-            const email = document.getElementById('email');
-            const emailNew = email.value.trim();
+            const emailNew = document.getElementById('email');
+            const email = emailNew.value.trim();
         
-            const userName = document.getElementById('username');
-            const userNameNew = userName.value.trim();
+            const userNameNew = document.getElementById('username');
+            const userName = userNameNew.value.trim();
         
             let isValid = true; // helps to track validation state, instead of using return statement in each condition
             const messages = []; // without an html element initialized with empty, string it can display error message to warn the client
         
-            if(userNameNew.length < 3) {
+            if(userName.length < 3) {
                 isValid = false; // shouldn't use 'let' to assign a new value, if used it will creat a new local scope
                 messages.push('Username is not valid!');
                 // return; // if validation fails, it won't show any error message because messages.push stores the error message inside the array, not show it to the user before the return statement unlike the other form validation code
             }
         
-            if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(emailNew)) {
+            if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
                 isValid = false;
                 messages.push('Invalid email format!');
             }
         
-            if (passwordNew.length < 8) {
+            if (password.length < 8) {
                 isValid = false;
                 messages.push('PAssword must be at least 8 characters long!');
             }
