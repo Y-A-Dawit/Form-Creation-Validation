c4e94712393286c22e2d12aa67ad116750b0c0f2
Yohannes Dawit <yohannesabreham08@gmail.com>
2025-02-17 12:16:48 +0300

    update

--- fetch-data.js
+++ fetch-data.js
@@ -1,11 +1,22 @@
+document.addEventListener('DOMContentLoaded', fetchUserData());
 const fetchUserData = async() => {
     const apiUrl = 'https://jsonplaceholder.typicode.com/users';
     
     const dataContainer = document.getElementById('api-data');
+
     try {
         const response = await fetch(apiUrl);
-        const jsonUserData = await response.json();
+        const users = await response.json();
+        dataContainer.innerHTML = '';
+        const userList = '<ul>';
+        users.forEach(user => {
+            const li = document.createElement('li') // here "createElement" is a dom property that creates elements
+            li.textContent = users.name;
+            userList.appendChild(li)
+        })
+        dataContainer.appendChild(userList);
     } catch {
-
+        dataContainer.innerHTML = '';
+        dataContainer.textContent = 'Failed to load user data.'
     }
 }
