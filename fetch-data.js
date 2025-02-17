c5eb451fe78a4f3d82d270035180620c072b1392
Yohannes Dawit <yohannesabreham08@gmail.com>
2025-02-17 11:24:42 +0300

    update

--- fetch-data.js
+++ fetch-data.js
@@ -1,3 +1,11 @@
 const fetchUserData = async() => {
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users';
     
+    const dataContainer = document.getElementById('api-data');
+    try {
+        const response = await fetch(apiUrl);
+        const jsonUserData = await response.json();
+    } catch {
+
+    }
 }
